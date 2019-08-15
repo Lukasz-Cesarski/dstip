f4aabea4b3da865483d5bf0e08f22d0c11a4b252
Lukasz-Cesarski <lukasz.cesarski@gmail.com>
2019-08-15 09:00:04 +0200

    Minor Bug fixing

--- dstip_utils/utils.py
+++ dstip_utils/utils.py
@@ -1,6 +1,7 @@
 from IPython.display import HTML, display
 from itertools import zip_longest
 
+"""
 def yes_no_table(yes_no_dict):
     data = [["When TO USE", "When NOT TO USE"]]
     for yes_arg, no_arg in zip_longest(yes_no_dict['yes'], yes_no_dict['no'], fillvalue=''):
@@ -11,3 +12,4 @@ def yes_no_table(yes_no_dict):
        '</tr><tr>'.join(
            '<td>{}</td>'.format('</td><td>'.join(str(_) for _ in row)) for row in data)
        )))
+"""
